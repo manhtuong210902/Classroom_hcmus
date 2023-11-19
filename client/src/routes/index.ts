cc661e0d9bd7fd65c10f9a70d954300482dafb4e
20120619 <mtuong669@gmail.com>
2023-11-19 10:57:55 +0700

    refactor: apply Layout to routes

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,16 +1,18 @@
+import HomeLayout from "@src/layouts/HomeLayout";
 import HomePage from "@src/pages/HomePage";
 import LandingPage from "@src/pages/LandingPage";
-import {EditProfilePage} from "@src/pages/Profile/index";
+import { EditProfilePage } from "@src/pages/Profile/index";
 
 type routeInfo = {
     path: string;
     component: React.FC;
+    layout?: React.FC;
 };
 
 const publicRoutes: routeInfo[] = [
     { path: "/", component: LandingPage },
-    { path: "/home", component: HomePage },
-    { path: "/profile", component: EditProfilePage},
+    { path: "/home", component: HomePage, layout: HomeLayout },
+    { path: "/profile", component: EditProfilePage, layout: HomeLayout },
 ];
 
 export { publicRoutes };
