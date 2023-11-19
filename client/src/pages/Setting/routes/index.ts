e4fce71e4ae5c150850b2f908493eed886769508
20120619 <mtuong669@gmail.com>
2023-11-19 11:11:52 +0700

    fix: fix confict

--- client/src/pages/Setting/routes/index.ts
+++ client/src/pages/Setting/routes/index.ts
@@ -1,8 +1,7 @@
-import HomeLayout from "@src/layouts/HomeLayout";
 import { EditProfilePage } from "..";
 
 const prefix = "setting";
 
-const settingRoutes: routeInfo[] = [{ path: `${prefix}/profile`, component: EditProfilePage, layout: HomeLayout }];
+const settingRoutes: routeInfo[] = [{ path: `${prefix}/profile`, component: EditProfilePage }];
 
 export default settingRoutes;
