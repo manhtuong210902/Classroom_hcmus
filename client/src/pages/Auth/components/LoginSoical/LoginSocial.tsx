cce9e60ee9751317fc6c950b2570a959601be8e3
20120619 <mtuong669@gmail.com>
2023-11-28 21:10:53 +0700

    config api login gg and fb

--- client/src/pages/Auth/components/LoginSoical/LoginSocial.tsx
+++ client/src/pages/Auth/components/LoginSoical/LoginSocial.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@src/components/ui/button";
+import { loginWithGoogle } from "@src/services/auth/apiRequest";
 import { Facebook, Mail } from "lucide-react";
 
 const LoginSocial = () => {
+    const handleLoginGoogle = () => {
+        console.log("Log check login gg");
+        loginWithGoogle();
+    };
+
     return (
         <div className="mt-6 text-primary flex flex-col gap-4">
-            <Button className="w-full text-base border-[2px]" variant="outline">
+            <Button className="w-full text-base border-[2px]" variant="outline" onClick={handleLoginGoogle}>
                 <Mail className="mr-2" size={16} />
                 Log in with Google
             </Button>
--- client/src/services/auth/apiRequest.ts
+++ client/src/services/auth/apiRequest.ts
@@ -3,6 +3,7 @@ import { LocalStorage } from "@src/utils/LocalStorage";
 import { profileService } from "../profile/profile.service";
 import { UserInfo } from "@src/utils/types";
 import { authService } from "./auth.service";
+import { AUTH_GOOGLE } from "./api";
 
 export const loaderUser = async (dispatch: any) => {
     const userId = LocalStorage.getUserId();
@@ -69,6 +70,10 @@ export const loginUser = async (dispatch: any, params: any) => {
         dispatch(loadUserFail());
         return error.response.data;
     }
+};
+
+export const loginWithGoogle = async () => {
+    window.open(AUTH_GOOGLE, "Login via Google", "width=400,height=600");
 };
 
 export const logoutUser = (dispatch: any) => {
