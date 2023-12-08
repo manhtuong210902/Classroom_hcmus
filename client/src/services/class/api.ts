b0467d872ee3444aa935fbc02eeca0b4105539d5
tun2g <tunghcltt@gmail.com>
2023-12-08 11:28:36 +0700

    [stag] call API

---
+++ client/src/services/class/api.ts
@@ -0,0 +1,5 @@
+import { ROUTE } from "../axiosClient";
+import { CLASS, PREFIX } from "../endpoints";
+
+export const LIST_CLASS_API = `${ROUTE}/${PREFIX}/${CLASS}/user-classes`;
+export const LIST_USER_IN_CLASS_API = `${ROUTE}/${PREFIX}/${CLASS}/users`;
