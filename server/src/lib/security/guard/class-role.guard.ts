ba1d0444ac314bd597a0d730aee8b025ee7cc3db
tun2g <tunghcltt@gmail.com>
2023-12-11 00:15:09 +0700

    [dev][server] fix class middleware

--- server/src/lib/security/guard/class-role.guard.ts
+++ server/src/lib/security/guard/class-role.guard.ts
@@ -3,21 +3,28 @@ import { Reflector } from '@nestjs/core';
 import { Role } from 'src/modules/role/role.entity';
 
 const matchRoles = (classRoles: string[], userRoles: string[]) => {
-  return userRoles.some(userRole => classRoles.includes(userRole));
+    return userRoles.some((userRole) => classRoles.includes(userRole));
 };
 
 @Injectable()
 export class ClassRolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const classRoles = this.reflector.get<string[]>('class-roles', context.getHandler());
-    if (!classRoles) {
-      return true;
+    canActivate(context: ExecutionContext): boolean {
+        try {
+            const classRoles = this.reflector.get<string[]>(
+                'class-roles',
+                context.getHandler(),
+            );
+            if (!classRoles) {
+                return true;
+            }
+            const req = context.switchToHttp().getRequest() as any;
+            const userRoles = req.classRoles;
+
+            return matchRoles(classRoles, userRoles);
+        } catch (error) {
+            return false;
+        }
     }
-    const req = context.switchToHttp().getRequest() as any;
-    const userRoles = req.classRoles
-    
-    return matchRoles(classRoles, userRoles);
-  }
 }
