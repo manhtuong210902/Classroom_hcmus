be8bbb681a6e25d27241b751db215ed6cb4e389a
tun2g <tunghcltt@gmail.com>
2024-01-12 14:25:11 +0700

    feat: api active class/user

--- server/src/modules/admin/admin.controller.ts
+++ server/src/modules/admin/admin.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, DefaultValuePipe, Get, HttpCode, HttpStatus, Inject, Post, Query, Req } from '@nestjs/common';
+import { BadRequestException, Body, Controller, DefaultValuePipe, Get, HttpCode, HttpStatus, Inject, Post, Query, Req } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { Role } from 'src/lib/security/decorators/role.decorator';
 import { RoleType } from 'src/lib/util/constant';
 import { ResponseTemplate } from 'src/lib/interfaces/response.template';
+import { ActivateUserDto } from './dto/activate-user.dto';
+import { ActivateClassDto } from './dto/activate-class.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -36,7 +38,17 @@ export class AdminController {
     async  getClass(
         @Query() query
     ){
-
+        try {            
+            const listClass = await this.adminService.getClass()
+            const response : ResponseTemplate<Object>={
+                data: listClass,
+                message: 'Successfully',
+                statusCode: 200,
+            } 
+            return response;
+        } catch (error) {
+            throw new BadRequestException();   
+        }
     } 
 
     @HttpCode(HttpStatus.OK)
@@ -47,12 +59,58 @@ export class AdminController {
         @Query('role', new DefaultValuePipe('')) role : string,
         @Query('sort_by', new DefaultValuePipe('')) sortBy: string,
     ){
-        const listUser = await this.adminService.getUser()
-        const response : ResponseTemplate<Object>={
-            data: listUser,
-            message: 'Successfully',
-            statusCode: 200,
-        } 
-        return response;
+        try {            
+            const listUser = await this.adminService.getUser()
+            const response : ResponseTemplate<Object>={
+                data: listUser,
+                message: 'Successfully',
+                statusCode: 200,
+            } 
+            return response;
+        } catch (error) {
+            throw new BadRequestException();   
+        }
     } 
+
+    @HttpCode(HttpStatus.OK)
+    @Post('/activate/user')
+    @Role(RoleType.ADMIN)
+    async activateUser(
+        @Req() req,
+        @Body() activateUserDto : ActivateUserDto
+    ){
+        try {
+            const adminId = req.user.id;
+            await this.adminService.activateUser(adminId, activateUserDto)
+            const response : ResponseTemplate<null> = {
+                data: null,
+                message: "Successfully",
+                statusCode: HttpStatus.OK
+            }
+            return response;
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('/activate/class')
+    @Role(RoleType.ADMIN)
+    async activateClass(
+        @Req() req,
+        @Body() activateClassDto : ActivateClassDto
+    ){
+        try {
+            const adminId = req.user.id;
+            await this.adminService.activateClass(adminId, activateClassDto)
+            const response : ResponseTemplate<null> = {
+                data: null,
+                message: "Successfully",
+                statusCode: HttpStatus.OK
+            }
+            return response;
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
 }
--- server/src/modules/admin/admin.service.ts
+++ server/src/modules/admin/admin.service.ts
@@ -5,6 +5,8 @@ import sequelize from 'sequelize';
 import { UserRole } from '../user/entities/user-role.entity';
 import { RoleService } from '../role/role.service';
 import { RoleType } from 'src/lib/util/constant';
+import { ActivateUserDto } from './dto/activate-user.dto';
+import { ActivateClassDto } from './dto/activate-class.dto';
 
 @Injectable()
 export class AdminService {
@@ -34,17 +36,105 @@ export class AdminService {
     }
 
     async getClass(){
-
+        try {
+            
+            const list = await this.ativeClassModel.sequelize.query(
+                `
+                SELECT classes.*,
+                    CASE WHEN COUNT(ac.class_id) = 0 THEN true ELSE false END AS is_active
+                FROM classes
+                LEFT JOIN 
+                    active_classes AS ac ON classes.id = ac.class_id AND ac.is_applied = true
+                GROUP BY classes.id;
+                `,
+                {
+                    type: sequelize.QueryTypes.SELECT
+                }
+            )
+            return list;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async getUser(){
         const list = await this.activeUserModel.sequelize.query(
-            `SELECT * FROM users;`,
+            `
+            SELECT
+                users.id,
+                users.username,
+                users.fullname,
+                users.address,
+                users.gender,
+                users.is_verified,
+                users.img_url,
+                users.created_at,
+                CASE WHEN COUNT(au.user_id) = 0 THEN true ELSE false END AS is_active
+            FROM
+                users
+            LEFT JOIN
+                active_users AS au ON users.id = au.user_id AND au.is_applied = true
+            GROUP BY
+                users.id;
+            `,
             {
                 type: sequelize.QueryTypes.SELECT
             }
         )
         return list;
+    }
+
+
+    async activateUser(
+        adminId: string, 
+        activateUserDto: ActivateUserDto
+    ){
+        if(activateUserDto.ban){
+            await this.activeUserModel.create({
+                user_id: activateUserDto.userId,
+                banned_by: adminId
+            })
+        }
+        else {
+            await this.activeUserModel.sequelize.query(
+                `
+                UPDATE active_users
+                SET is_applied = FALSE
+                WHERE user_id = :userId;
+                `,
+                {
+                    replacements: {
+                        userId: activateUserDto.userId
+                    }
+                }
+            )
+        }
+    }
+
+    async activateClass(
+        adminId: string, 
+        activateClassDto: ActivateClassDto
+    ){
+        if(activateClassDto.ban){
+            await this.activeUserModel.create({
+                user_id: activateClassDto.classId,
+                banned_by: adminId
+            })
+        }
+        else {
+            await this.activeUserModel.sequelize.query(
+                `
+                UPDATE active_users
+                SET is_applied = FALSE
+                WHERE class_id = :classId;
+                `,
+                {
+                    replacements: {
+                        classId: activateClassDto.classId
+                    }
+                }
+            )
+        }
     }
 
 }
