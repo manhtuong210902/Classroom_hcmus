ec4a230ef1df3b29b8507eb6a306467676140341
tun2g <tunghcltt@gmail.com>
2024-01-20 22:33:27 +0700

    add migrations

---
+++ server/src/database/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "ntt24082002",
+    "database": "convert_excel",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+  },
+}
---
+++ server/src/database/migrations/20240120152111-init.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.addColumn(
+      'users',
+      'hashed_token',
+      {
+        type: Sequelize.STRING,
+        allowNull: true, 
+      }
+    )
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
