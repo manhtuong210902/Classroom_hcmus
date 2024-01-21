9a832cba544d26399d2f7c6d8b95a60c240ae64e
tun2g <tunghcltt@gmail.com>
2024-01-21 14:46:29 +0700

    fix

--- server/src/database/config/database.js
+++ server/src/database/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "ntt24082002",
-    "database": "convert_excel",
+    "username": "",
+    "password": "",
+    "database": "",
     "host": "127.0.0.1",
     "dialect": "postgres",
   },
